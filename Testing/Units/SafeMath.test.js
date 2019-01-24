feef2c1b09bed838e4ef64a495baa588c364b6da
Trevor Stanley <38226475+T-Man-Stan@users.noreply.github.com>
2019-01-24 15:26:09 -0700

    Add files via upload

---
+++ Testing/Units/SafeMath.test.js
@@ -0,0 +1,131 @@
+const SafeMathMock = artifacts.require('./SafeMathMock.sol');
+
+const BigNumber = web3.BigNumber;
+
+const assertRevert = require('../Utilities/test.util').assertRevert;
+
+contract('SafeMath', function () {
+	const MAX_UINT256 = new BigNumber(2).pow(256).minus(1);
+	const MAX_UINT16 = new BigNumber(2).pow(16).minus(1);
+
+	let sut;
+
+	beforeEach(async function () {
+		sut = await SafeMathMock.new();
+	});
+
+	describe('sub16', function () {
+		it('subtracts correctly', async function () {
+			// Arrange
+			const a = new BigNumber(5678);
+			const b = new BigNumber(1234);
+			// Act
+			const result = await sut.sub16.call(a, b);
+			// Assert
+			assert.deepEqual(result, a.minus(b));
+		});
+
+		it('throws a revert error if subtraction result would be negative', async function () {
+			// Arrange
+			const a = new BigNumber(1234);
+			const b = new BigNumber(5678);
+			// Act
+			// Assert
+			await assertRevert(sut.sub16(a, b));
+		});
+	});
+
+	describe('add16', function () {
+		it('adds correctly', async function () {
+			// Arrange
+			const a = new BigNumber(5678);
+			const b = new BigNumber(1234);
+			// Act
+			const result = await sut.add16.call(a, b);
+			// Assert
+			assert.deepEqual(result, a.plus(b));
+		});
+
+		it('throws a revert error on addition overflow', async function () {
+			// Arrange
+			const a = MAX_UINT16;
+			const b = new BigNumber(1);
+			// Act
+			// Assert
+			await assertRevert(sut.add16(a, b));
+		});
+	});
+
+	describe('mul', function () {
+		it('multiplies correctly', async function () {
+			// Arrange
+			const a = new BigNumber(1234);
+			const b = new BigNumber(5678);
+			// Act
+			const result = await sut.mul.call(a, b);
+			// Assert
+			assert.deepEqual(result, a.times(b));
+		});
+
+		it('handles a zero product correctly', async function () {
+			// Arrange
+			const a = new BigNumber(0);
+			const b = new BigNumber(5678);
+			// Act
+			const result = await sut.mul.call(a, b);
+			// Assert
+			assert.deepEqual(result, a.times(b));
+		});
+
+		it('throws a revert error on multiplication overflow', async function () {
+			// Arrange
+			const a = MAX_UINT256;
+			const b = new BigNumber(2);
+			// Act
+			// Assert
+			await assertRevert(sut.mul(a, b));
+		});
+	});
+
+	describe('sub', function () {
+		it('subtracts correctly', async function () {
+			// Arrange
+			const a = new BigNumber(5678);
+			const b = new BigNumber(1234);
+			// Act
+			const result = await sut.sub.call(a, b);
+			// Assert
+			assert.deepEqual(result, a.minus(b));
+		});
+
+		it('throws a revert error if subtraction result would be negative', async function () {
+			// Arrange
+			const a = new BigNumber(1234);
+			const b = new BigNumber(5678);
+			// Act
+			// Assert
+			await assertRevert(sut.sub(a, b));
+		});
+	});
+
+	describe('add', function () {
+		it('adds correctly', async function () {
+			// Arrange
+			const a = new BigNumber(5678);
+			const b = new BigNumber(1234);
+			// Act
+			const result = await sut.add.call(a, b);
+			// Assert
+			assert.deepEqual(result, a.plus(b));
+		});
+
+		it('throws a revert error on addition overflow', async function () {
+			// Arrange
+			const a = MAX_UINT256;
+			const b = new BigNumber(1);
+			// Act
+			// Assert
+			await assertRevert(sut.add(a, b));
+		});
+	});
+});
---
+++ Testing/Units/UnInitOwn.test.js
@@ -0,0 +1,78 @@
+const UnInitOwn = artifacts.require('./UnInitOwn.sol');
+
+const assertRevert = require('../Utilities/test.util').assertRevert;
+const inLogs = require('../Utilities/test.util').inLogs;
+
+contract('UnInitOwn', function ([coinbase, another]) {
+	let sut;
+	const ZERO_ADDRESS = '0x' + '0'.repeat(40);
+
+	beforeEach(async function () {
+		sut = await UnInitOwn.new();
+	});
+
+	it('Should not have owner When instantiated', async function () {
+		// Arrange
+		// Act
+		const owner = await sut.owner.call();
+		// Assert
+		assert.equal(owner, ZERO_ADDRESS);
+	});
+
+	it('init Should set owner When called', async function () {
+		// Arrange
+		await sut.init();
+		// Act
+		const owner = await sut.owner.call();
+		// Assert
+		assert.equal(owner, coinbase);
+	});
+
+	it('init Should revert When called with already set owner', async function () {
+		// Arrange
+		await sut.init();
+		// Act
+		const result = sut.init();
+		// Assert
+		await assertRevert(result);
+	});
+
+	it('transferOwnership Should transfer the ownership When passed valid arguments', async function () {
+		// Arrange
+		await sut.init();
+		const initialOwner = await sut.owner.call();
+		assert.equal(initialOwner, coinbase);
+		// Act
+		await sut.transferOwnership(another);
+		const owner = await sut.owner.call();
+		// Assert
+		assert.equal(owner, another);
+	});
+
+	it('transferOwnership Should raise LogOwnershipTransferred event When passed valid arguments', async function () {
+		// Arrange
+		await sut.init();
+		// Act
+		const { logs } = await sut.transferOwnership(another);
+		// Assert
+		await inLogs(logs, 'LogOwnershipTransferred', { previousOwner: coinbase, newOwner: another });
+	});
+
+	it('transferOwnership Should revert When the passed `_newOwner` account equals 0', async function () {
+		// Arrange
+		await sut.init();
+		// Act
+		const result = sut.transferOwnership(ZERO_ADDRESS);
+		// Assert
+		await assertRevert(result);
+	});
+
+	it('transferOwnership Should revert When called from non-owner account', async function () {
+		// Arrange
+		await sut.init();
+		// Act
+		const result = sut.transferOwnership(another, { from: another });
+		// Assert
+		await assertRevert(result);
+	});
+});
