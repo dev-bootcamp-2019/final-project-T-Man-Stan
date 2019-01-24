a2ca7f917b5d9b2e3b2ae34ac3f92cc5d23ed294
Trevor Stanley <38226475+T-Man-Stan@users.noreply.github.com>
2019-01-24 14:43:26 -0700

    Create Paused.sol

---
+++ contracts/Process/Paused.sol
@@ -0,0 +1,39 @@
+pragma solidity 0.4.24;
+
+import '../owners/StoreOwn.sol';
+
+
+/**
+ * @title Paused
+ * @dev Base contract which allows children to implement an emergency stop mechanism.
+ */
+contract Paused is StoreOwn {
+	event LogPause();
+	event LogUnpause();
+
+	bool public paused = false;
+
+	/**
+	* @dev Modifier to make a function callable only when the contract is not paused.
+	*/
+	modifier whenNotPaused() {
+		require(!paused, 'The contract must not be paused!');
+		_;
+	}
+
+	/**
+	* @dev called by the owner to pause, triggers stopped state
+	*/
+	function pause() public onlyOwner {
+		emit LogPause();
+		paused = true;
+	}
+
+	/**
+	* @dev called by the owner to unpause, returns to normal state
+	*/
+	function unpause() public onlyOwner {
+		emit LogUnpause();
+		paused = false;
+	}
+}
