6c66208e82db3f361b072213f30bfa2570d3493b
Trevor Stanley <38226475+T-Man-Stan@users.noreply.github.com>
2019-01-24 14:42:45 -0700

    Add files via upload

---
+++ contracts/owners/StoreOwn.sol
@@ -0,0 +1,89 @@
+pragma solidity 0.4.24;
+
+/**
+ * @title IMarketplace
+ * @dev Interface to the marketplace's transferStoreOwnership
+ * function used for transferring stores ownership
+ */
+interface IMarketplace {
+	function transferStoreOwnership(address _storeOwner, uint16 _storeIndex, address _newStoreOwner, uint16 _newOwnerStoreIndex) external;
+}
+
+
+/**
+ * @title StoreOwn
+ * @dev The StoreOwnable contract holds owner and marketplace addresses,
+ * and provides basic authorization control and control transferring
+ * functionality.
+ */
+contract StoreOwn {
+
+	event LogOwnershipTransferRequested(address indexed currentOwner, address indexed ownerCandidate, uint256 storeIndex);
+	event LogOwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+	address public owner;
+	address public marketplace;
+	
+	address public ownerCandidate;
+	uint256 public storeIndex;
+
+	/**
+	* @dev Throws if called by any account other than the current owner.
+	*/
+	modifier onlyOwner() {
+		require(msg.sender == owner, 'The msg sender must be the owner');
+		_;
+	}
+
+	/**
+	* @dev Throws if called by any account other than the owner candidate.
+	*/
+	modifier onlyOwnerCandidate() {
+		require(msg.sender == ownerCandidate, 'The msg sender must be the owner candidate');
+		_;
+	}
+
+	/**
+	* @dev Throws if called by any account other than the marketplace.
+	*/
+	modifier onlyMarketplace() {
+		require(msg.sender == marketplace, 'The msg sender must be the marketplace');
+		_;
+	}
+
+	/**
+	* @dev Throws if called by any account other than
+	* the owner or the marketplace.
+	*/
+	modifier onlyOwnerOrMarketplace() {
+		require(msg.sender == owner || msg.sender == marketplace, 'The msg sender must be the owner or the marketplace');
+		_;
+	}
+
+	/**
+	* @dev Allows the current owner to transfer control of the contract to a new owner, who has to approve the ownership.
+	* @notice The requirement for non-zero address of the `_ownerCandidate`
+	* is intentionally omitted due to the two-staged implementation.
+	* @param _ownerCandidate The address to transfer ownership to.
+	* @param _storeIndex The index of the current store to be transferred.
+	*/
+	function requestOwnershipTransfer(address _ownerCandidate, uint256 _storeIndex) public onlyOwner {
+		emit LogOwnershipTransferRequested(owner, _ownerCandidate, _storeIndex);
+		ownerCandidate = _ownerCandidate;
+		storeIndex = _storeIndex;
+	}
+
+	/**
+	* @dev Allows owner candidate to approve the ownership of the contract.
+	* @param _newStoreIndex The index at which the new store to be set
+	* in the new owner's stores array.
+	*/
+	function approveOwnershipTransfer(uint16 _newStoreIndex) public onlyOwnerCandidate {
+		emit LogOwnershipTransferred(owner, ownerCandidate);
+
+		IMarketplace m = IMarketplace(marketplace);
+		m.transferStoreOwnership(owner, uint16(storeIndex), ownerCandidate, _newStoreIndex);
+
+		owner = ownerCandidate;
+	}
+}
---
+++ contracts/owners/UnInitOwn.sol
@@ -0,0 +1,40 @@
+pragma solidity 0.4.24;
+
+/**
+ * @title UnInitOwn
+ * @dev The NotInitedOwnable contract has an owner address,
+ * and provides basic authorization control functions,
+ * which simplifies the implementation of "user permissions".
+ * @notice This contract needs to be inited after deployment for proper use.
+ */
+contract UnInitOwn {
+	address public owner;
+
+	event LogOwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+	/**
+	 * @dev Reverts if called by any account other than the owner's.
+	 */
+	modifier onlyOwner() {
+		require(msg.sender == owner, 'Msg sender is not an owner');
+		_;
+	}
+
+	/**
+	* @dev Initializes the msg sender as an owner account.
+	*/
+	function init() public {
+		require(owner == address(0), 'Contract already has owner');
+		owner = msg.sender;
+	}
+
+	/**
+	 * @dev Allows the current owner to transfer control of the contract to a new owner.
+	 * @param _newOwner The address to transfer ownership to.
+	 */
+	function transferOwnership(address _newOwner) public onlyOwner {
+		require(_newOwner != address(0), '_newOwner address can not be 0');
+		emit LogOwnershipTransferred(owner, _newOwner);
+		owner = _newOwner;
+	}
+}
